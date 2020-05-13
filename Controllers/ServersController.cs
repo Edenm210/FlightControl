b8dcb0ac8c21050df4f20fba74dce769678afce4
odeya-shnaps <59564743+odeya-shnaps@users.noreply.github.com>
2020-05-13 19:19:31 +0300

    added controllers and models

---
+++ Controllers/ServersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlightControlWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightControlWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServersController : ControllerBase
+    {
+        private IDataManagementModel model;
+
+        public ServersController(IDataManagementModel m)
+        {
+            this.model = m;
+            //model.AddDatabase(db);
+        }
+
+        // GET: api/Servers
+        [HttpGet]
+        public List<Server> GetAllServers()
+        {
+            return model.GetAllServers();
+        }
+
+        // POST: api/Servers
+        [HttpPost]
+        public void Post([FromBody] Server value)
+        {
+            model.AddServer(value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            model.DeleteServer(id);
+        }
+    }
+}
---
+++ Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+    public class Location
+    {
+        public Location(double longi, double lati)
+        {
+            Longitude = longi;
+            Latitude = lati;
+        }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}

99c38a94d33a6a956c7e72fca7aa26b3fee8e472
ora amir <ora.amir@live.biu.ac.il>
2020-05-18 21:59:58 +0300

    added a button to upload flight plans

--- FlightControlWeb/wwwroot/js/MainWindow.js
+++ FlightControlWeb/wwwroot/js/MainWindow.js
@@ -56,7 +56,45 @@ function creatingPushpin(location, imgUrl, scale, callback) {
 }
 
 
+/*
+ * start of code that uploads a new flight to the server
+ */
+const fileSelect = document.getElementById("fileSelect"),
+    fileElem = document.getElementById("fileElem");
+fileElem.addEventListener("change", handleFiles, false);
 
+/*when the button is pushed, it calls the event that the fileElem button was pushed,
+so we can upload a new file*/
+fileSelect.addEventListener("click", function (e) {
+    if (fileElem) {
+        fileElem.click();
+    }
+}, false);
 
-   
+/*send the file uploaded to the server*/
+function handleFiles() {
+    let file = fileElem.files[0]; //the flight to upload
+    let url = "/api/FlightPlan";
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4) { //http sent and answer recieved
+            if (this.status != 200) { //error accured
+                //error accured. need to respond!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            }
+        }
+    };
 
+    let reader = new FileReader();
+    reader.readAsText(file); //read the file
+    reader.onload = function () { //send the content to the server
+        xmlhttp.open("POST", url, true);
+        xmlhttp.setRequestHeader("content-type", "application/json");
+        xmlhttp.send(reader.result);
+    };
+    reader.onerror = function () {
+        //take care of error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+    };
+}
+/*
+* end of code that uploads a new flight to the server
+*/

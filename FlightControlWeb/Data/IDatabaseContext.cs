469100647c2b0da6fc59d5d21573dd6b7033cf2f
odeya-shnaps <59564743+odeya-shnaps@users.noreply.github.com>
2020-05-25 21:28:07 +0300

    add db interface

---
+++ FlightControlWeb/Data/IDatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using FlightControlWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Data
+{
+    public interface IDatabaseContext
+    {
+        Task<List<FlightPlan>> GetAllFlightPlans();
+        Task<FlightPlan> GetFlightPlan(string id);
+        Task<string> AddFlightPlan(FlightPlan fp);
+        Task<bool> DeleteFlightPlan(string id);
+        Task<List<Server>> GetServers();
+        Task AddServer(Server server);
+        Task<bool> DeleteServer(string id);
+        List<FlightWithServer> LoadAllFlightsWithServers();
+        Task AddFlightsFromServers(List<FlightWithServer> fws);
+        Task DeleteFlightsFromServer(string serverURL);
+        Task<Server> GetServer(string id);
+    }
+}
